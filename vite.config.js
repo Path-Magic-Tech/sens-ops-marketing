4c27a688da421ed775662b0bcd828cca8caf7747
con-sou <conorsouhrada@icloud.com>
2025-10-29 21:54:17 -0500

    attempting pages resolution

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/sens-ops-marketing', 
+  base: '/sens-ops-marketing/', 
 })
